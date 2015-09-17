2fc8f802851b946f8052dd13c92d81ac569612b1
Grummfy <me@grummfy.be>
2015-09-17 18:18:38 +0200

    code transaction split

--- src/Transformation/TransformationToSimpleAdvanced.php
+++ src/Transformation/TransformationToSimpleAdvanced.php
@@ -6,24 +6,20 @@ use Codelicious\Coda\Data;
 
 class TransformationToSimpleAdvanced extends TransformToSimple
 {
+	public function __construct()
+	{
+		$this->setSimpleObjectsDefinition(array(
+			self::CLASS_TRANSACTION => '\Codelicious\Coda\Data\Simple\Advanced\Transaction',
+		));
+	}
+
 	public function transformTransaction(Data\Raw\Transaction $coda_transaction)
 	{
-		$transaction = new Data\Simple\Advanced\Transaction();
-		$transaction->account = $this->transformToOtherPartyAccount($coda_transaction->line22, $coda_transaction->line23);
-
-		if ($coda_transaction->line21) {
-			$transaction->valuta_date = $coda_transaction->line21->valuta_date;
-			$transaction->transaction_date = $coda_transaction->line21->transaction_date;
-			$transaction->amount = $coda_transaction->line21->amount;
-		}
-
-		if ($coda_transaction->line21 && $coda_transaction->line21->structured_message) {
-			$transaction->structured_message = $coda_transaction->line21->structured_message;
-		} elseif ($coda_transaction->line31 && $coda_transaction->line31->structured_message) {
-			$transaction->structured_message = $coda_transaction->line31->structured_message;
-		}
-
-		$transaction->message = $this->concatenateTransactionMessages($coda_transaction);
+		$transaction = parent::transformTransaction($coda_transaction);
+		$transaction->codeType = substr($coda_transaction->line21->transaction_code, 0, 1);
+		$transaction->codeFamily = substr($coda_transaction->line21->transaction_code, 1, 2);
+		$transaction->codeTransaction = substr($coda_transaction->line21->transaction_code, 3, 2);
+		$transaction->codeCategory = substr($coda_transaction->line21->transaction_code, 5, 3);
 
 		return $transaction;
 	}
