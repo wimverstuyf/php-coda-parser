18e0956f33f53cc88fba5998969a48c5e5c931aa
Grummfy <me@grummfy.be>
2015-09-17 18:06:23 +0200

    transaction tu use transsaction code

--- src/Transformation/TransformationToSimpleAdvanced.php
+++ src/Transformation/TransformationToSimpleAdvanced.php
@@ -6,30 +6,27 @@ use Codelicious\Coda\Data;
 
 class TransformationToSimpleAdvanced extends TransformToSimple
 {
-//	public function transformTransaction(Data\Raw\Transaction $coda_transaction)
-//	{
-//		return parent::transformTransaction($coda_transaction);
-//		/*
-//		$transaction = new Transaction();
-//		$transaction->account = $this->transformToOtherPartyAccount($coda_transaction->line22, $coda_transaction->line23);
-//
-//		if ($coda_transaction->line21) {
-//			$transaction->valuta_date = $coda_transaction->line21->valuta_date;
-//			$transaction->transaction_date = $coda_transaction->line21->transaction_date;
-//			$transaction->amount = $coda_transaction->line21->amount;
-//		}
-//
-//		if ($coda_transaction->line21 && $coda_transaction->line21->structured_message) {
-//			$transaction->structured_message = $coda_transaction->line21->structured_message;
-//		} elseif ($coda_transaction->line31 && $coda_transaction->line31->structured_message) {
-//			$transaction->structured_message = $coda_transaction->line31->structured_message;
-//		}
-//
-//		$transaction->message = $this->concatenateTransactionMessages($coda_transaction);
-//
-//		return $transaction;
-//		*/
-//	}
+	public function transformTransaction(Data\Raw\Transaction $coda_transaction)
+	{
+		$transaction = new Data\Simple\Advanced\Transaction();
+		$transaction->account = $this->transformToOtherPartyAccount($coda_transaction->line22, $coda_transaction->line23);
+
+		if ($coda_transaction->line21) {
+			$transaction->valuta_date = $coda_transaction->line21->valuta_date;
+			$transaction->transaction_date = $coda_transaction->line21->transaction_date;
+			$transaction->amount = $coda_transaction->line21->amount;
+		}
+
+		if ($coda_transaction->line21 && $coda_transaction->line21->structured_message) {
+			$transaction->structured_message = $coda_transaction->line21->structured_message;
+		} elseif ($coda_transaction->line31 && $coda_transaction->line31->structured_message) {
+			$transaction->structured_message = $coda_transaction->line31->structured_message;
+		}
+
+		$transaction->message = $this->concatenateTransactionMessages($coda_transaction);
+
+		return $transaction;
+	}
 
 	public function concatenateTransactionMessages(Data\Raw\Transaction $coda_transaction)
 	{
